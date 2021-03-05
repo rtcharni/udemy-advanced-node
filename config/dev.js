0168d01b926199e318b9acc104f92a6f8f2a4ab8
Roman Tcharni <roman.tcharni@gmail.com>
2021-03-05 21:55:34 +0200

    npm i, added mongo db url

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,8 @@
 module.exports = {
   googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123',
+    "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
+  googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
+  mongoURI:
+    "mongodb://dbUser:g8Wqk5nS7QDfBMkf@udemyadvancednodecluste-shard-00-00.b4gnm.mongodb.net:27017,udemyadvancednodecluste-shard-00-01.b4gnm.mongodb.net:27017,udemyadvancednodecluste-shard-00-02.b4gnm.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-he8u0a-shard-0&authSource=admin&retryWrites=true&w=majority",
+  cookieKey: "123123123",
 };
