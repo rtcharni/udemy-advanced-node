757bfdd7b54ba258d54af9eac964406b7a1655d3
Roman Tcharni <roman.tcharni@gmail.com>
2021-03-06 11:46:34 +0200

    redis cache implemented

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
-
+const redis = require('redis');
+const redisUrl = 'redis://localhost:6379';
+const client = redis.createClient(redisUrl);
+const util = require('util');
+client.get = util.promisify(client.get);
+const clearCache = require('../middlewares/clearHash');
 const Blog = mongoose.model('Blog');
 
 module.exports = app => {
@@ -14,12 +19,11 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
-
+    const blogs = await Blog.find({ _user: req.user.id }).cache({ key: req.user.id });
     res.send(blogs);
   });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post('/api/blogs', requireLogin, clearCache, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
