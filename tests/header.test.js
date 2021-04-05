8c4217274ecca811929c0579f23c22b2dcb5551f
Roman Tcharni <roman.tcharni@gmail.com>
2021-04-05 20:33:27 +0300

    test works

---
+++ tests/header.test.js
@@ -0,0 +1,32 @@
+const CustomPage = require('./helpers/page');
+
+let page;
+
+beforeEach(async () => {
+  page = await CustomPage.build();
+  await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+  await page.close();
+});
+
+test('the header has correct text', async () => {
+  const text = await page.getContentsOf('a.brand-logo');
+  expect(text).toEqual('Blogster');
+});
+
+test('clicking login starts oAuth flow', async () => {
+  await page.click('.right a');
+
+  const url = await page.url();
+
+  expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('show logout button after login', async () => {
+  await page.login();
+  const text = await page.getContentsOf('a[href="/auth/logout"]');
+
+  expect(text).toEqual('Logout');
+});
---
+++ tests/setup.js
@@ -0,0 +1,8 @@
+jest.setTimeout(20000);
+
+require('../models/User');
+const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, { useMongoClient: true });
